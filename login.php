86620a1846b0d3f12a37b4f657907b90887854f3
Pedro Galan <pedrogalanagu1@gmail.com>
2024-06-07 17:36:29 +0200

    Conexión con la bd Creada 4.1

--- login.php
+++ login.php
@@ -12,6 +12,24 @@
     <h1>Inicio de Sesión</h1>
     <input type="text" name="usuario" placeholder="Nombre">
     <input type="password" name="contraseña" placeholder="Contraseña">
+    <?php
+        $host='localhost';
+        $dbname='control_fct';
+        $user='root';
+        $pass='';
+        try{
+         //verifica que en el array POST no está vacío y que el índice inicioEnviar se encuentra en el osea que el boton se ha puldado en el formulario
+         if(!empty($_POST['inicioEnviar'])){
+                $pdo=new PDO("mysql:host=$host;dbname=$dbname;charset=utf8",$user,$pass);
+              $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+         }
+
+         }catch(PDOException $e){
+            echo "Se ha producido un error al intentar conectar el servidor SQL";
+        }
+
+    ?>
     <input id="submit" type="submit" name="inicioEnviar" value="Iniciar Sesion">
     </form>
 </body>
