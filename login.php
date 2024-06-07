7a923735fce2f962a615eb5fc92e699ad82eeea3
Pedro Galan <pedrogalanagu1@gmail.com>
2024-06-07 16:02:50 +0200

    HTML Básico de la página

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+    <form method="POST">
+    <img src="logo__.svg" alt="Logo mal">
+    <h1>Inicio de Sesión</h1>
+    <input type="text" name="usuario" placeholder="Nombre">
+    <input type="password" name="contraseña" placeholder="Contraseña">
+    <input type="submit" name="inicioEnviar" value="Iniciar Sesion">
+    </form>
+</body>
+</html>
