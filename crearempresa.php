0c13a0d08985de8a37120e82273ad2973200d656
Pedro Galan <pedrogalanagu1@gmail.com>
2024-06-10 03:44:16 +0200

    CSS de crear empresa e implementaciones de salir

--- crearempresa.php
+++ crearempresa.php
@@ -3,31 +3,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.5.0/font/bootstrap-icons.min.css">
+
+    <link rel="stylesheet" type="text/css" href="crear.css">
     <title>Document</title>
 </head>
 <body>
     <h1>Crear nueva Empresa</h1>
     <form action="crearempresa.php" method="POST">
+        <a href="crudtutor.php"><i class="bi bi-x-lg bi-3x"></i></a>
         <label for="nombre">Nombre:</label>
         <input type="text" name="nombre">
+        
         <label for="cif">Cif:</label>
         <input type="text" name="cif">
+        
         <label for="nombre_fiscal">Nombre Fiscal:</label>
         <input type="text" name="nombre_fiscal">
+        
         <label for="email">Email:</label>
         <input type="text" name="email">
+        
         <label for="direccion">Dirección:</label>
         <input type="text" name="direccion">
+        
         <label for="localidad">Localidad:</label>
         <input type="text" name="localidad">
+        
         <label for="provincia">Provincia:</label>
         <input type="text" name="provincia">
+        
         <label for="numero_plazas">Numero de Plazas:</label>
         <input type="text" name="numero_plazas">
+        
         <label for="telefono">Telefono:</label>
         <input type="text" name="telefono">
+
         <label for="persona_contacto">Persona de Contacto:</label>
         <input type="text" name="persona_contacto">
+
+        <input type="submit" value="Crear">
     </form>
 
 
